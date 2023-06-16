7374c87ae0faf97171575cfe69866d2e2a3c20be
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-16 20:59:39 -0300

    Add ex01

--- exercicio1.js
+++ exercicio1.js
@@ -6,3 +6,18 @@
  * adulto (18-59 anos) ou idoso (60 anos ou mais). Utiliza uma estrutura condicional
  * (if, else if, else).
  */
+
+let idade = 10
+if (idade > 0 && idade <= 12) {
+    console.log("É uma criança.")
+} else if (idade >= 13 && idade <= 17) {
+    console.log("É um adolescente.")
+} else if (idade >= 18 && idade <= 59) {
+    console.log("É um adulto")
+} else if (idade >=60 && idade < 120) {
+    console.log("É um idoso.")
+} else if (idade > 120) {
+    console.log("Tá de brincadeira? É o terror do INSS.")
+} else {
+    console.log("Número inválido.")
+}
