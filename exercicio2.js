22c4c31127130ec0fc479fc5611cda0fe351047f
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-16 21:06:07 -0300

    Add ex02

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,21 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+window.alert("Escolha uma cor primária.")
+
+let primaryColor = "Yellow"
+
+switch (primaryColor) {
+    case "Yellow":
+        console.log("Amarelo")
+        break;
+    case "Blue":
+        console.log("Azul")
+        break;
+    case "Red":
+        console.log("Vermelho")
+        break;
+    default:
+        console.log("Não é uma cor primária!")    
+}
