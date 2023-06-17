d71b6f5095e8217d39ead19d8a4ebcf879c93216
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-16 23:42:15 -0300

    Add ex05

--- exercicio5.js
+++ exercicio5.js
@@ -4,3 +4,15 @@
  * Escreve um programa que percorre um array de palavras e exibe todas as palavras que possuem
  * mais de 5 caracteres. Utiliza a estrutura de repetição for of para percorrer o array.
  */
+
+let cars = ['gol', 'ka', 'hb20', 'fiesta', 'sandero', '208', 'polo', 'elantra', 'passat']
+let words = []
+let cut = []
+
+for (cut of cars){
+    if (cut.length>=5){
+        words.push(cut)
+    }
+}
+
+console.log(words)
