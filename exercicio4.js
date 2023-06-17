7fa66378c14c13b71ce27aa32c38e4662c03fcc0
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-06-16 23:06:45 -0300

    Add ex04

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,15 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+let numero = 200
+let i1 = 0
+let i2 = 1
+let i3 = 0
+
+while (i1 < numero) {
+    console.log(i1)
+    i3 = i1 + i2;
+    i2 = i1
+    i1 = i3
+}
